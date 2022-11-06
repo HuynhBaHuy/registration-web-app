62de5a08a407101e1fd13f55e80b64e831ebdb4d
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-11-06 17:16:37 +0700

    change post to get

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,43 +1,49 @@
 // @ts-nocheck
 import { Button } from "antd";
-import React, { useEffect } from "react";
-import { useMutation } from "react-query";
+import React from "react";
+import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import constants from "../../constants";
 function Home() {
     const navigate = useNavigate();
 
-    const mutation = useMutation((token) => {
-        console.log("Token", token);
-        return fetch(`${constants.apiConfig.DOMAIN_NAME}${constants.apiConfig.ENDPOINT.profile}`, {
-            method: constants.apiConfig.methods.get,
-            headers: {
-                "Content-type": "application/json",
-                "x-access-token": token
-            },
-        }).then((response) => {
-            return response.json();
-        })
-    });
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (!token) {
-            navigate("/login");
-        } else {
-            mutation.mutate(token);
+    const queries = useQuery({
+        queryKey: "users",
+        queryFn: () => {
+            const token = localStorage.getItem("token");
+            console.log("Token", token);
+            return fetch(`${constants.apiConfig.DOMAIN_NAME}${constants.apiConfig.ENDPOINT.profile}`, {
+                method: constants.apiConfig.methods.get,
+                headers: {
+                    "Content-type": "application/json",
+                    "x-access-token": token
+                },
+            }).then((response) => {
+                return response.json();
+            })
+        },
+        onSuccess: (data) => {
+            if(data?.code===200){
+                console.log(data);
+            }else{
+                navigate("/login");
+            }
+        },
+        onError: (error) => {
+            console.log("Error", error);
         }
-    }, [navigate]);
+    });
 
     return (
         <div>
             <h1>Home</h1>
-            {mutation.isLoading && <p>Loading...</p>}
-            {mutation.isError && <p>Error</p>}
-            {mutation.isSuccess && (
+            {queries.isLoading && <p>Loading...</p>}
+            {queries.isError && <p>Error</p>}
+            {queries?.data?.data && (
                 <div>
-                    <p>Full name: {mutation.data?.data?.fullName}</p>
-                    <p>Email: {mutation.data?.data?.email}</p>
-                    <Button onClick={()=> {
+                    <p>Full name: {queries.data?.data?.fullName}</p>
+                    <p>Email: {queries.data?.data?.email}</p>
+                    <Button onClick={() => {
                         localStorage.removeItem("token");
                         navigate("/login");
                     }}>Logout</Button>
